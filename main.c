082405833b00de80d244b0a381ce2abc48846490
Faruk Caki <contact@ofarukcaki.com>
2019-12-28 16:05:26 +0300

    Create lineCount()

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+
+int lineCount(char *filename) {
+    FILE *fp;
+    int count = 0;  // Line counter (result)
+    char c;
+    // Open the file
+    fp = fopen(filename, "r");
+
+    // Check if file exists
+    if (fp == NULL) {
+        printf("Could not open file %s", filename);
+        return 0;
+    }
+
+    // Extract characters from file and store in character c
+    for (c = getc(fp); c != EOF; c = getc(fp))
+        if (c == '\n') // Increment count if this character is newline
+            count = count + 1;
+
+    // Close the file
+    fclose(fp);
+    return count;
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+    lineCount("test.txt");
     return 0;
 }
