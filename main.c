7b44ef828ffca6291a99baf9e521aa5da3009178
O.Faruk <omerfarukcaki@gmail.com>
2019-12-30 10:08:18 +0300

    Create getReadNum()

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include <pthread.h>
 #include <unistd.h>
 
-
-struct _list {
+struct _list
+{
     char *text;
     int lineNum;
-    int upper;  // uppered or not
-    int replace;    // replaced or not
-    struct list *next;   // next node
+    int upper;         // uppered or not
+    int replace;       // replaced or not
+    struct list *next; // next node
 };
 /* Example list node
  *  {
@@ -22,15 +22,17 @@ struct list *root;
 
 char *b = "W0rld";
 
-int lineCount(char *filename) {
+int lineCount(char *filename)
+{
     FILE *fp;
-    int count = 0;  // Line counter (result)
+    int count = 0; // Line counter (result)
     char c;
     // Open the file
     fp = fopen(filename, "r");
 
     // Check if file exists
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         printf("Could not open file %s", filename);
         return 0;
     }
@@ -45,7 +47,8 @@ int lineCount(char *filename) {
     return count;
 }
 
-char *getLine(char *file, int lineNum) {
+char *getLine(char *file, int lineNum)
+{
 
     // TODO: Return the nth line from the file
     FILE *fp;
@@ -57,8 +60,10 @@ char *getLine(char *file, int lineNum) {
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
-    while ((getline(&line, &len, fp)) != -1) {
-        if (i == lineNum) {
+    while ((getline(&line, &len, fp)) != -1)
+    {
+        if (i == lineNum)
+        {
             return strdup(line);
         }
         i++;
@@ -68,18 +73,40 @@ char *getLine(char *file, int lineNum) {
     if (line)
         free(line);
 
-
     fclose(fp);
     return NULL;
 }
 
-void *hello(void *arg) {
+void *hello(void *arg)
+{
     printf("Hello %s\n", b);
     return NULL;
 }
 
+int limit = 5;
+int current = 0;
+pthread_mutex_t mutexRead;
 
-int main() {
+// determine the line to be read by a thread
+int getReadNum()
+{
+    int rv;
+    if (current > limit)
+        return -1;
+    pthread_mutex_lock(&mutexRead);
+
+    rv = current;
+    current++;
+    pthread_mutex_unlock(&mutexRead);
+    return rv;
+}
+
+void *tl(void *args)
+{
+}
+
+int main()
+{
     int readThreadCount = 3;
     int count = lineCount("test.txt");
 
@@ -87,10 +114,22 @@ int main() {
 
     printf("Line %d: %s\n", 0, getLine("test.txt", 0));
 
+    pthread_mutex_init(&mutexRead, NULL);
+
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
+    printf("GetReadnum: %d\n", getReadNum());
 
     pthread_t newThread;
     pthread_create(&newThread, NULL, &hello, NULL);
+
     pthread_join(newThread, NULL);
-//    sleep(5);
+    //    sleep(5);
     return 0;
 }
